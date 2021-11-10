9b4a95cb928325eb9fc1a2a7e7ec7454c3e734c1
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2021-11-09 22:53:23 -0600

    change color

--- choices/static/main/dice.js
+++ choices/static/main/dice.js
@@ -1,9 +1,19 @@
 import { get_face } from './show_choice.js'
 
-var $die = $('.die'),
+// Need to have switch here to hide dice
+// Need to have swtich to choose advantage or disadvantage
+// There are three modes
+// normal, advantage, and disadvantage
+// Normal mode, one dice is used
+// Advantage mode, two dices are used and bigger face is used
+// Disadvantage mode, two dices are used and smaller face is used
+
+var $die_1 = $('#die-1'),
+    $die_2 = $('#die-2'),
     sides = 20,
     initialSide = 1,
-    lastFace,
+    face_1,
+    face_2,
     timeoutId,
     transitionDuration = 100, 
     animationDuration  = 1000
@@ -15,35 +25,43 @@ $('ul > li > a').click(function () {
   return false
 })
 
-function randomFace() {
-  var face = Math.floor((Math.random() * sides)) + initialSide
-  lastFace = face == lastFace ? randomFace() : face
-  return lastFace;
+function randomFace(face) {
+  var newFace = Math.floor((Math.random() * sides)) + initialSide
+  face = newFace == face ? randomFace(face) : newFace
+  return face;
 }
 
-function rollTo(face) {
+function rollTo() {
   clearTimeout(timeoutId)
   
   $('ul > li > a').removeClass('active')
-  $('[href=' + face + ']').addClass('active')
+  $('[href=' + face_1 + ']').addClass('active')
+  $('[href=' + face_2 + ']').addClass('active')
   
-  $die.attr('data-face', face)
+  $die_1.attr('data-face', face_1)
+  $die_2.attr('data-face', face_2)
 }
 
 function reset() {
-  $die.attr('data-face', null).removeClass('rolling')
+  $die_1.attr('data-face', null).removeClass('rolling')
+  $die_2.attr('data-face', null).removeClass('rolling')
 }
 
 $('.randomize, .die').click(function () {
-  $die.addClass('rolling')
+  $die_1.addClass('rolling')
+  $die_2.addClass('rolling')
   clearTimeout(timeoutId)
   
   timeoutId = setTimeout(function () {
-    $die.removeClass('rolling')
+    $die_1.removeClass('rolling')
+    $die_2.removeClass('rolling')
     
-    let face = randomFace()
-    rollTo(face)
-    get_face(face)
+    face_1 = randomFace(face_1)
+    face_2 = randomFace(face_2)
+    rollTo()
+    
+    // specify the mode here
+    get_face(face_1)
   }, animationDuration)
   
   return false
