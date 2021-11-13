ed57c2eb8968a3da032b4071a7f2249b2b33ecb5
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2021-11-12 19:29:06 -0600

    beatify choice box

--- choices/static/main/show_choice.js
+++ choices/static/main/show_choice.js
@@ -3,12 +3,22 @@ import {all_choice_list} from './get_all_dom_as_array.js'
 var focus = ''
 var face = ''
 
+const user_choice = document.getElementById("your-choice");
+
+const change_user_choice_text = (new_value) => {
+    user_choice.innerText = new_value
+}
+
 const _show_choice = (target) => {
     for (let i=0; i<all_choice_list.length; i++){
         if ( all_choice_list[i].id === target){
+            const new_value = all_choice_list[i].innerText
+            change_user_choice_text(new_value)
             all_choice_list[i].style.color = 'white'
+            all_choice_list[i].style.backgroundColor = 'black'
         }else{
             all_choice_list[i].style.color = 'black'
+            all_choice_list[i].style.backgroundColor = 'white'
         }
     }
 }
